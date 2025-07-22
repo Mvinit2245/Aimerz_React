db97d8ee2cf7acbcacc5a4dfb5e0116ba58a03aa
Mvinit2245 <vinitmorankar22@gmail.com>
2025-07-22 14:50:39 +0530

    React-Query commit

--- third-project/src/App.jsx
+++ third-project/src/App.jsx
@@ -1,26 +1,52 @@
 import axios from 'axios'
-import { useEffect, useState } from 'react'
+//import { useEffect, useState, useQuery } from 'react'
 import './App.css'
+import { QueryClient, QueryClientProvider, useQuery } from '@tanstack/react-query';
 
-function App() {
-  const [quote, setQuote] = useState('')
-
+function Quote() {
   async function fetcheQuote() {
-    const response = await axios.get('https://dummyjson.com/quotes/random' )
-    console.log(response.data.quote);
-    setQuote(response.data.quote)
+    try {
+      const response = await axios({
+        method: "GET",
+        url: "https://dummyjson.com/quotes/random",
+      });
+      return response.data.quote;
+    } catch (err) {
+      console.log(err);
+    }
+
   }
 
- useEffect(() =>{
-  fetcheQuote();
- } ,[])
-  
+  const { data, isLoading, error } = useQuery({
+    queryKey: ['quote'],
+    queryFn: fetcheQuote,
+  });
+  // if (isLoading) return <p>Loading...</p>;
+  // if (error) return <p>Error loading quote</p>;
+
+  return <p>{isLoading ? 'Loading' : data}</p>;
+}
+
+function App() {
+  // const [quote, setQuote] = useState('')
+  const queryClient = new QueryClient();
+
+
+
+  //  useEffect(() =>{
+  //   fetcheQuote();
+  //  } ,[])
+
   return (
-        <div className='p-5'>
-          <h1 className='text-3xl text-center mb-5'>Random Quote Generator </h1>
-          <p>{quote ? quote : 'Loading'}</p>
-          </div>
-      
+    <div className='p-5'>
+      <h1 className='text-3xl text-center mb-5'>Random Quote Generator </h1>
+      {/* <p>{quote ? quote : 'Loading'}</p> */}
+
+      <QueryClientProvider client={queryClient}>
+        <Quote />
+      </QueryClientProvider>
+    </div>
+
   )
 }
 
